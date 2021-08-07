1ed75471739fed7793650cf9fcbb1cb9225cf344
mgxc2 <mgxc2@MacBook-Pro-cua-mgxc2.local>
2021-08-07 18:10:18 +0700

    first commit

--- api/submission/services/submission.js
+++ api/submission/services/submission.js
@@ -5,31 +5,4 @@
  * to customize this service
  */
 
-module.exports = {
-  async create(data, { files } = {}) {
-    const validData = await strapi.entityValidator.validateEntityCreation(
-      strapi.models.submission,
-      data
-    );
-    const allSubmission = await strapi.query("submission").find();
-    if (allSubmission.filter((e) => e.email === validData.email).length !== 0) {
-      return { error: "Just one postcard per user, please" };
-    }
-    if (allSubmission.length > 150) {
-      return { error: "Sorry we have got enough 150 submissions" };
-    } else {
-      const entry = await strapi.query("submission").create(validData);
-      const error = await strapi.services.utils.sendGrid(data, "WELCOME");
-      if (error) {
-        return { error: error.response.body.errors[0].message };
-      }
-      if (files) {
-        await strapi.entityService.uploadFiles(entry, files, {
-          model: "submission",
-        });
-        return this.findOne({ id: entry.id });
-      }
-      return entry;
-    }
-  },
-};
+module.exports = {};
--- api/utils/services/utils.js
+++ api/utils/services/utils.js
@@ -10,23 +10,5 @@ const MAIL_TEMPLATES = {
 };
 
 module.exports = {
-  sendGrid: async (data, MAIL_NAME) => {
-    const template = MAIL_TEMPLATES[MAIL_NAME];
-    sgMail.setApiKey(process.env.SEND_GRID_KEY);
-    const msg = {
-      to: data.email, // Change to your recipient
-      from: process.env.ACCOUNT_SENDER, // Change to your verified sender
-      subject: template.subject,
-      html: await ejs.renderFile(
-        path.resolve(__dirname, template.ejsContent),
-        data
-      ),
-    };
-
-    try {
-      await sgMail.send(msg);
-    } catch (error) {
-      return error;
-    }
-  },
+  sendGrid: async (data, MAIL_NAME) => {},
 };
